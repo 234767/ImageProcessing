d07e503ae4503e66c840778fa0b8ee26c3122550
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-10-14 14:32:09 +0200

    Added contrast adjustment

--- src/modifications.rs
+++ src/modifications.rs
@@ -1,6 +1,6 @@
-use crate::parsing::Args as Args;
-use num;
+use crate::parsing::Args;
 use image::{Pixel, RgbImage};
+use num;
 
 pub trait Transformation {
     fn apply<'a>(&self, image: &'a mut RgbImage) -> &'a mut RgbImage;
@@ -10,6 +10,7 @@ pub fn get_transformation(args: &Args) -> Result<Box<dyn Transformation>, String
     match args.command.as_str() {
         "--negative" => Ok(Box::new(Negative {})),
         "--brightness" => Ok(Box::new(Brightness::try_new(args)?)),
+        "--contrast" => Ok(Box::new(Contrast::try_new(args)?)),
         _ => Err(format!("Command {} undefined", args.command)),
     }
 }
@@ -26,7 +27,7 @@ impl Transformation for Negative {
 }
 
 pub struct Brightness {
-    amount: i32
+    amount: i32,
 }
 
 impl Brightness {
@@ -45,7 +46,33 @@ impl Brightness {
 impl Transformation for Brightness {
     fn apply<'a>(&self, image: &'a mut RgbImage) -> &'a mut RgbImage {
         for (_x, _y, pixel) in image.enumerate_pixels_mut() {
-            *pixel = pixel.map(|c| num::clamp(c as i32 + self.amount,0,u8::MAX as i32) as u8);
+            *pixel = pixel.map(|c| num::clamp(c as i32 + self.amount, 0, u8::MAX as i32) as u8);
+        }
+        image
+    }
+}
+
+struct Contrast {
+    factor: f64,
+}
+
+impl Contrast {
+    pub fn try_new(args: &Args) -> Result<Self, String> {
+        let amount = args.args.get("-amount");
+        match amount {
+            Some(amount) => match amount.parse::<f64>() {
+                Ok(factor) if factor >= 0.0 => Ok(Self { factor }),
+                _ => Err(format!("Amount {} is not a positive number", amount)),
+            },
+            None => Err(String::from("Missing -amount argument")),
+        }
+    }
+}
+
+impl Transformation for Contrast {
+    fn apply<'a>(&self, image: &'a mut RgbImage) -> &'a mut RgbImage {
+        for (_x, _y, pixel) in image.enumerate_pixels_mut() {
+            *pixel = pixel.map(|c| num::clamp((c as f64 - 128f64) * self.factor + 128f64, 0f64, u8::MAX as f64) as u8);
         }
         image
     }
