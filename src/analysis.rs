e6a521eb15dcde597f85f6128b047b92098c4fd3
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-02 19:44:55 +0100

    Refactoring

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,12 +1,13 @@
 use crate::parsing::Args;
-use image::RgbImage;
+use image::{Rgb, RgbImage};
+use num::Num;
 
 use analyzers::*;
 
 mod analyzers;
 
 pub trait Analyzer {
-    fn compare(&self, original: &RgbImage, other: &RgbImage) -> Result<String, String>;
+    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String>;
 }
 
 pub fn get_analyzers(args: &Args) -> Box<dyn Analyzer> {
@@ -30,11 +31,36 @@ impl CompositeAnalyzer {
 }
 
 impl Analyzer for CompositeAnalyzer {
-    fn compare(&self, original: &RgbImage, other: &RgbImage) -> Result<String, String> {
+    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String> {
         let mut result = String::new();
         for analyzer in &self.analyzers {
-            result.push_str(&analyzer.compare(original, other)?);
+            result.push_str(&analyzer.compare(original, modified)?);
         }
         Ok(result)
     }
+}
+
+/// For each channel of RGB maps a given function, and sums the results
+///
+/// # Arguments
+///
+/// * `original`: original image
+/// * `modified`: modified image
+/// * `function`: function to map the different brightness values
+///
+/// returns: Rgb<i128>
+/// Values for RGB channels respectively
+fn map_and_sum<F>(original: &RgbImage, modified: &RgbImage, function: F) -> Rgb<i128>
+where
+    F: Fn(u8, u8) -> i128,
+{
+    let mut total: [i128; 3] = [0, 0, 0];
+    for (x, y, original_pixel) in original.enumerate_pixels() {
+        let modified_pixel = modified.get_pixel(x, y);
+        for channel in 1..3 {
+            let value = function(original_pixel[channel], modified_pixel[channel]);
+            total[channel] += value;
+        }
+    }
+    Rgb(total)
 }
