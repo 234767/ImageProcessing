21e1daa88eef1f8de0aed14aa91519833b25dfc9
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-02 15:17:41 +0100

    Moved particular analyzers to separate file

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,6 +1,10 @@
 use crate::parsing::Args;
 use image::RgbImage;
 
+use analyzers::*;
+
+mod analyzers;
+
 pub trait Analyzer {
     fn compare(&self, original: &RgbImage, other: &RgbImage) -> Result<String, String>;
 }
@@ -32,24 +36,5 @@ impl Analyzer for CompositeAnalyzer {
             result.push_str(&analyzer.compare(original, other)?);
         }
         Ok(result)
-    }
-}
-
-struct MeanSquareError {}
-
-impl Analyzer for MeanSquareError {
-    fn compare(&self, original: &RgbImage, other: &RgbImage) -> Result<String, String> {
-        let mut total: [i64; 3] = [0, 0, 0];
-        for (x, y, original_pixel) in original.enumerate_pixels() {
-            let other_pixel = other.get_pixel(x, y);
-            for channel in 1..3 {
-                let difference: i64 =
-                    (original_pixel[channel] as i64) - (other_pixel[channel] as i64);
-                total[channel] += difference * difference;
-            }
-        }
-        let result: f64 = (total.iter().sum::<i64>() as f64)
-            / (3.0 * original.width() as f64 * original.height() as f64);
-        Ok(format!("MSE: {}", result))
     }
 }
