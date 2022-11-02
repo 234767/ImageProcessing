c85ce1490d41f8a8c73be0565613b022e2bc0809
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-02 20:24:00 +0100

    Implemented max difference

--- src/analysis/analyzers.rs
+++ src/analysis/analyzers.rs
@@ -1,6 +1,7 @@
+use crate::analysis::util::{map_and_reduce, map_and_sum, DoubleImageIterator};
 use crate::analysis::Analyzer;
 use image::{Rgb, RgbImage};
-use crate::analysis::util::map_and_sum;
+use std::cmp::max;
 
 pub struct MeanSquareError {}
 
@@ -13,5 +14,25 @@ impl Analyzer for MeanSquareError {
         let result: f64 = (totals.iter().sum::<i128>() as f64)
             / (3.0 * original.width() as f64 * original.height() as f64);
         Ok(format!("MSE: {}", result))
+    }
+}
+
+pub struct MaximumDifference {}
+
+impl Analyzer for MaximumDifference {
+    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String> {
+        let Rgb(results) = map_and_reduce(
+            original,
+            modified,
+            |old, new| old as i128 - new as i128,
+            max,
+            Rgb([0, 0, 0]),
+        );
+
+        let result_opt = results.iter().max();
+        match result_opt {
+            Some(result) => Ok(format!("MD: {}", result)),
+            None => Err(String::from("Could not compute maximum difference"))
+        }
     }
 }
