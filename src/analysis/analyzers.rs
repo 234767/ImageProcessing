5797730e3202c2f2b51169010f9380e400449b43
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-03 20:43:52 +0100

    Refactored analyzers to use static methods

--- src/analysis/analyzers.rs
+++ src/analysis/analyzers.rs
@@ -5,22 +5,29 @@ use std::cmp::max;
 
 pub struct MeanSquareError {}
 
-impl Analyzer for MeanSquareError {
-    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String> {
+impl MeanSquareError {
+    fn compare(original: &RgbImage, modified: &RgbImage) -> f64 {
         let Rgb(totals) = map_and_sum(original, modified, |a, b| {
             let difference = a as i128 - b as i128;
             difference * difference
         });
         let result: f64 = (totals.iter().sum::<i128>() as f64)
             / (3.0 * original.width() as f64 * original.height() as f64);
+        result
+    }
+}
+
+impl Analyzer for MeanSquareError {
+    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String> {
+        let result = Self::compare(original, modified);
         Ok(format!("MSE: {}", result))
     }
 }
 
 pub struct MaximumDifference {}
 
-impl Analyzer for MaximumDifference {
-    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String> {
+impl MaximumDifference {
+    fn compare(original: &RgbImage, modified: &RgbImage) -> Option<i128> {
         let Rgb(results) = map_and_reduce(
             original,
             modified,
@@ -29,7 +36,13 @@ impl Analyzer for MaximumDifference {
             Rgb([0, 0, 0]),
         );
 
-        let result_opt = results.iter().max();
+        results.iter().max().map(|c| c.clone())
+    }
+}
+
+impl Analyzer for MaximumDifference {
+    fn compare(&self, original: &RgbImage, modified: &RgbImage) -> Result<String, String> {
+        let result_opt = Self::compare(original, modified);
         match result_opt {
             Some(result) => Ok(format!("MD: {}", result)),
             None => Err(String::from("Could not compute maximum difference"))
