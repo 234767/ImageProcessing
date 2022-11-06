9405211fca0027f130ad998cc708186263107462
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-06 20:27:41 +0100

    Removed duplicate code

--- src/modifications/filters.rs
+++ src/modifications/filters.rs
@@ -11,6 +11,30 @@ fn is_in_range(x: u32, y: u32, image: &RgbImage) -> bool {
     x < image.width() && y < image.height()
 }
 
+fn collect_pixels(image: &RgbImage, x: u32, x_offset: u32, y: u32, y_offset: u32) -> Vec<&Rgb<u8>> {
+    let mut old_pixels: Vec<&Rgb<u8>> = Vec::new();
+    for x in u32::saturating_sub(x, x_offset)..(x + y_offset) {
+        for y in u32::saturating_sub(y, y_offset)..(y + y_offset) {
+            if is_in_range(x, y, image) {
+                old_pixels.push(image.get_pixel(x, y));
+            }
+        }
+    }
+    old_pixels
+}
+
+fn get_width_and_height(args: &Args) -> Result<(u32,u32), String> {
+    let mut width: u32 = args.try_get_arg("-w")?;
+    if width.is_even() {
+        width += 1
+    }
+    let mut height: u32 = args.try_get_arg("-h")?;
+    if height.is_even() {
+        height += 1
+    }
+    Ok((width,height))
+}
+
 //(N1) Median filter (--median)
 pub struct MedianFilter {
     width: u32,
@@ -19,14 +43,7 @@ pub struct MedianFilter {
 
 impl MedianFilter {
     pub fn try_new(args: &Args) -> Result<Self, String> {
-        let mut width: u32 = args.try_get_arg("-w")?;
-        if width.is_even() {
-            width += 1
-        }
-        let mut height: u32 = args.try_get_arg("-h")?;
-        if height.is_even() {
-            height += 1
-        }
+        let (width, height) = get_width_and_height(args)?;
         Ok(Self { width, height })
     }
 }
@@ -37,14 +54,7 @@ impl Transformation for MedianFilter {
         let height_offset = self.height / 2;
         let mut new_image: RgbImage = ImageBuffer::new(image.width(), image.height());
         for (target_x, target_y, new_pixel) in new_image.enumerate_pixels_mut() {
-            let mut old_pixels: Vec<&Rgb<u8>> = Vec::new();
-            for x in u32::saturating_sub(target_x, width_offset)..(target_x + width_offset) {
-                for y in u32::saturating_sub(target_y, height_offset)..(target_y + height_offset) {
-                    if is_in_range(x, y, image) {
-                        old_pixels.push(image.get_pixel(x, y));
-                    }
-                }
-            }
+            let old_pixels: Vec<&Rgb<u8>> = collect_pixels(image, target_x, width_offset, target_y, height_offset);
             for channel in 0..3 {
                 let mut luminosities: Vec<u8> =
                     old_pixels.iter().map(|pixel| pixel[channel]).collect();
@@ -64,34 +74,20 @@ pub struct GeometricMeanFilter {
 
 impl GeometricMeanFilter {
     pub fn try_new(args: &Args) -> Result<Self, String> {
-        let mut width: u32 = args.try_get_arg("-w")?;
-        if width.is_even() {
-            width += 1
-        }
-        let mut height: u32 = args.try_get_arg("-h")?;
-        if height.is_even() {
-            height += 1
-        }
+        let (width, height) = get_width_and_height(args)?;
         Ok(Self { width, height })
     }
 }
 
 impl Transformation for GeometricMeanFilter {
     fn apply(&self, image: &mut RgbImage) {
-        let mut h_offset = self.height / 2;
-        let mut w_offset = self.width / 2;
+        let h_offset = self.height / 2;
+        let w_offset = self.width / 2;
         let mut new_image: RgbImage = ImageBuffer::new(image.width(), image.height());
         for (target_x, target_y, new_pixel) in new_image.enumerate_pixels_mut() {
-            let mut old_pixels: Vec<&Rgb<u8>> = Vec::new();
-            for x in u32::saturating_sub(target_x, w_offset)..(target_x + w_offset) {
-                for y in u32::saturating_sub(target_y, h_offset)..(target_y + h_offset) {
-                    if is_in_range(x, y, image) {
-                        old_pixels.push(image.get_pixel(x, y));
-                    }
-                }
-            }
+            let old_pixels: Vec<&Rgb<u8>> = collect_pixels(image, target_x, w_offset, target_y, h_offset);
             for channel in 0..3 {
-                let mut luminosities: Vec<u8> =
+                let luminosities: Vec<u8> =
                     old_pixels.iter().map(|pixel| pixel[channel]).collect();
                 new_pixel[channel] =
                     luminosities.iter().sum::<u8>() / luminosities.iter().count() as u8;
