aef93a6314e7b40822e380bb410a0e14c40f23c7
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-07 13:59:09 +0100

    Removed code duplication og GPU configuration

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,20 +1,30 @@
 use image::{DynamicImage, ImageBuffer, RgbImage, Rgba};
 use std::sync::Arc;
-use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::buffer::{BufferContents, BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferToImageInfo, CopyImageToBufferInfo,
+    PrimaryAutoCommandBuffer,
+};
+use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
 
 use vulkano::device::{Device, DeviceCreateInfo, Queue, QueueCreateInfo};
 use vulkano::instance::{Instance, InstanceCreateInfo};
 
 use vulkano::format::Format;
-use vulkano::image::{ImageDimensions, StorageImage, ImageCreationError};
-use vulkano::{VulkanLibrary};
+use vulkano::image::view::ImageView;
+use vulkano::image::{ImageCreationError, ImageDimensions, StorageImage};
+use vulkano::pipeline::{ComputePipeline, Pipeline, PipelineBindPoint};
+use vulkano::shader::ShaderModule;
+use vulkano::sync::GpuFuture;
+use vulkano::{sync, VulkanLibrary};
 
-pub struct GPUComputeRunner {
+#[derive(Clone)]
+pub struct GPUConfig {
     pub device: Arc<Device>,
     pub queue: Arc<Queue>,
 }
 
-impl GPUComputeRunner {
+impl GPUConfig {
     pub fn new() -> Option<Self> {
         let library = VulkanLibrary::new().expect("No local vulkan library");
         let instance = match Instance::new(
@@ -58,16 +68,172 @@ impl GPUComputeRunner {
     }
 }
 
-pub fn rgb_image_from_buffer(width: u32, height: u32, buffer: Arc<CpuAccessibleBuffer<[u8]>>) -> RgbImage {
-    let buffer_content = Vec::from(&buffer.read().unwrap()[..]);
-    DynamicImage::from(
-        ImageBuffer::<Rgba<u8>, _>::from_raw(width, height, buffer_content)
-            .unwrap(),
-    )
-    .to_rgb8()
+pub struct InOutImageTransformationPipeline {
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    image_width: u32,
+    image_height: u32,
+    out_buffer: Arc<CpuAccessibleBuffer<[u8]>>,
+    command_buffer: PrimaryAutoCommandBuffer,
 }
 
-pub fn create_image(device: Arc<Device>, queue: &Queue, width: u32, height: u32) -> Result<Arc<StorageImage>, ImageCreationError>{
+impl InOutImageTransformationPipeline {
+    pub fn new<Sb, Pc>(
+        config: GPUConfig,
+        src_image: &RgbImage,
+        shader_builder: Sb,
+        group_counts: [u32; 3],
+        push_constants: Option<Pc>,
+    ) -> InOutImageTransformationPipeline
+    where
+        Sb: Fn(Arc<Device>) -> Arc<ShaderModule>,
+        Pc: BufferContents,
+    {
+        let out_image = create_image(
+            config.device.clone(),
+            &*config.queue,
+            src_image.width(),
+            src_image.height(),
+        )
+        .unwrap();
+
+        let out_image_view = ImageView::new_default(out_image.clone()).unwrap();
+
+        let in_image = create_image(
+            config.device.clone(),
+            &*config.queue,
+            src_image.width(),
+            src_image.height(),
+        )
+        .unwrap();
+
+        let in_image_view = ImageView::new_default(in_image.clone()).unwrap();
+
+        let shader = shader_builder(config.device.clone());
+
+        let compute_pipeline = ComputePipeline::new(
+            config.device.clone(),
+            shader.entry_point("main").unwrap(),
+            &(),
+            None,
+            |_| {},
+        )
+        .expect("Failed to create compute pipeline");
+
+        let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+        let set = PersistentDescriptorSet::new(
+            layout.clone(),
+            [
+                WriteDescriptorSet::image_view(0, in_image_view.clone()),
+                WriteDescriptorSet::image_view(1, out_image_view.clone()),
+            ],
+        )
+        .unwrap();
+
+        let in_buffer: Arc<CpuAccessibleBuffer<[u8]>> = {
+            let input_rgba: Vec<u8> = DynamicImage::from(src_image.clone())
+                .to_rgba8()
+                .as_raw()
+                .clone();
+
+            CpuAccessibleBuffer::from_iter(
+                config.device.clone(),
+                BufferUsage {
+                    transfer_src: true,
+                    ..Default::default()
+                },
+                false,
+                input_rgba,
+            )
+            .unwrap()
+        };
+
+        let out_buffer = CpuAccessibleBuffer::from_iter(
+            config.device.clone(),
+            BufferUsage {
+                transfer_dst: true,
+                ..Default::default()
+            },
+            false,
+            (0..src_image.width() * src_image.height() * 4).map(|_| 0u8),
+        )
+        .unwrap();
+
+        let mut builder = AutoCommandBufferBuilder::primary(
+            config.device.clone(),
+            config.queue.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+
+        builder
+            .copy_buffer_to_image(CopyBufferToImageInfo::buffer_image(
+                in_buffer.clone(),
+                in_image.clone(),
+            ))
+            .unwrap()
+            .bind_pipeline_compute(compute_pipeline.clone())
+            .bind_descriptor_sets(
+                PipelineBindPoint::Compute,
+                compute_pipeline.layout().clone(),
+                0,
+                set,
+            );
+
+        if let Some(push_constants) = push_constants {
+            builder.push_constants(compute_pipeline.layout().clone(), 0, push_constants);
+        }
+
+        builder
+            .dispatch(group_counts)
+            .unwrap()
+            .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
+                out_image.clone(),
+                out_buffer.clone(),
+            ))
+            .unwrap();
+
+        let command_buffer = builder.build().unwrap();
+
+        Self {
+            device: config.device,
+            queue: config.queue,
+            image_width: src_image.width(),
+            image_height: src_image.height(),
+            out_buffer,
+            command_buffer,
+        }
+    }
+
+    pub(crate) fn dispatch(self) -> RgbImage {
+        let future = sync::now(self.device.clone())
+            .then_execute(self.queue.clone(), self.command_buffer)
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap();
+
+        future.wait(None).unwrap();
+
+        rgb_image_from_buffer(self.image_width, self.image_height, self.out_buffer)
+    }
+}
+
+pub fn rgb_image_from_buffer(
+    width: u32,
+    height: u32,
+    buffer: Arc<CpuAccessibleBuffer<[u8]>>,
+) -> RgbImage {
+    let buffer_content = Vec::from(&buffer.read().unwrap()[..]);
+    DynamicImage::from(ImageBuffer::<Rgba<u8>, _>::from_raw(width, height, buffer_content).unwrap())
+        .to_rgb8()
+}
+
+pub fn create_image(
+    device: Arc<Device>,
+    queue: &Queue,
+    width: u32,
+    height: u32,
+) -> Result<Arc<StorageImage>, ImageCreationError> {
     StorageImage::new(
         device.clone(),
         ImageDimensions::Dim2d {
--- src/modifications/filters/gpu_optimized.rs
+++ src/modifications/filters/gpu_optimized.rs
@@ -1,26 +1,13 @@
-use crate::gpu::{create_image, rgb_image_from_buffer, GPUComputeRunner};
+use crate::gpu::{GPUConfig, InOutImageTransformationPipeline};
 use crate::modifications::Transformation;
 use crate::parsing::Args;
-use image::{DynamicImage, RgbImage};
+use image::RgbImage;
 use num::Integer;
-use std::sync::Arc;
-use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
-use vulkano::command_buffer::{
-    AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferToImageInfo, CopyImageToBufferInfo,
-};
-use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
-use vulkano::device::{Device, Queue};
-use vulkano::image::view::ImageView;
-use vulkano::pipeline::{ComputePipeline, Pipeline, PipelineBindPoint};
-use vulkano::shader::ShaderModule;
-use vulkano::sync;
-use vulkano::sync::GpuFuture;
 
 pub struct MedianFilterGPU {
     x_radius: u32,
     y_radius: u32,
-    device: Arc<Device>,
-    queue: Arc<Queue>,
+    config: GPUConfig,
 }
 
 impl MedianFilterGPU {
@@ -34,14 +21,16 @@ impl MedianFilterGPU {
             height -= 1
         }
         if height * width > 400 {
-            return Err(format!("Values of height and width too large. Maximum sampling area is 400, got {}.", height*width));
+            return Err(format!(
+                "Values of height and width too large. Maximum sampling area is 400, got {}.",
+                height * width
+            ));
         }
-        if let Some(GPUComputeRunner { device, queue }) = GPUComputeRunner::new() {
+        if let Some(config) = GPUConfig::new() {
             Ok(Self {
                 x_radius: width / 2,
                 y_radius: height / 2,
-                device,
-                queue,
+                config,
             })
         } else {
             Err(String::from(
@@ -53,26 +42,6 @@ impl MedianFilterGPU {
 
 impl Transformation for MedianFilterGPU {
     fn apply(&self, image: &mut RgbImage) {
-        let out_image = create_image(
-            self.device.clone(),
-            &*self.queue,
-            image.width(),
-            image.height(),
-        )
-        .unwrap();
-
-        let out_image_view = ImageView::new_default(out_image.clone()).unwrap();
-
-        let in_image = create_image(
-            self.device.clone(),
-            &*self.queue,
-            image.width(),
-            image.height(),
-        )
-        .unwrap();
-
-        let in_image_view = ImageView::new_default(in_image.clone()).unwrap();
-
         mod cs {
             vulkano_shaders::shader! {
                 ty: "compute",
@@ -90,95 +59,15 @@ impl Transformation for MedianFilterGPU {
             y_radius: self.y_radius,
         };
 
-        let shader: Arc<ShaderModule> =
-            cs::load(self.device.clone()).expect("Failed to create shader module");
+        let pipeline = InOutImageTransformationPipeline::new(
+            self.config.clone(),
+            image,
+            |device| cs::load(device).expect("Failed to create shader module"),
+            [image.width() / 16, image.height() / 16, 1],
+            Some(push_constants),
+        );
 
-        let compute_pipeline = ComputePipeline::new(
-            self.device.clone(),
-            shader.entry_point("main").unwrap(),
-            &(),
-            None,
-            |_| {},
-        )
-        .expect("Failed to create compute pipeline");
-
-        let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
-        let set = PersistentDescriptorSet::new(
-            layout.clone(),
-            [
-                WriteDescriptorSet::image_view(0, in_image_view.clone()),
-                WriteDescriptorSet::image_view(1, out_image_view.clone()),
-            ],
-        )
-        .unwrap();
-
-        let out_buf = CpuAccessibleBuffer::from_iter(
-            self.device.clone(),
-            BufferUsage {
-                transfer_dst: true,
-                ..Default::default()
-            },
-            false,
-            (0..image.width() * image.height() * 4).map(|_| 0u8),
-        )
-        .unwrap();
-
-        let input_rgba: Vec<u8> = DynamicImage::from(image.clone())
-            .to_rgba8()
-            .as_raw()
-            .clone();
-
-        let in_buf = CpuAccessibleBuffer::from_iter(
-            self.device.clone(),
-            BufferUsage {
-                transfer_src: true,
-                ..Default::default()
-            },
-            false,
-            input_rgba,
-        )
-        .unwrap();
-
-        let mut builder = AutoCommandBufferBuilder::primary(
-            self.device.clone(),
-            self.queue.queue_family_index(),
-            CommandBufferUsage::OneTimeSubmit,
-        )
-        .unwrap();
-
-        builder
-            .copy_buffer_to_image(CopyBufferToImageInfo::buffer_image(
-                in_buf.clone(),
-                in_image.clone(),
-            ))
-            .unwrap()
-            .bind_pipeline_compute(compute_pipeline.clone())
-            .bind_descriptor_sets(
-                PipelineBindPoint::Compute,
-                compute_pipeline.layout().clone(),
-                0,
-                set,
-            )
-            .push_constants(compute_pipeline.layout().clone(), 0, push_constants)
-            .dispatch([image.width() / 16, image.height() / 16, 1])
-            .unwrap()
-            .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
-                out_image.clone(),
-                out_buf.clone(),
-            ))
-            .unwrap();
-
-        let command_buffer = builder.build().unwrap();
-
-        let future = sync::now(self.device.clone())
-            .then_execute(self.queue.clone(), command_buffer)
-            .unwrap()
-            .then_signal_fence_and_flush()
-            .unwrap();
-
-        future.wait(None).unwrap();
-
-        let result_image = rgb_image_from_buffer(image.width(), image.height(), out_buf);
+        let result_image = pipeline.dispatch();
 
         *image = result_image;
     }
@@ -187,8 +76,7 @@ impl Transformation for MedianFilterGPU {
 pub struct GMeanFilterGPU {
     x_radius: u32,
     y_radius: u32,
-    device: Arc<Device>,
-    queue: Arc<Queue>,
+    config: GPUConfig,
 }
 
 impl GMeanFilterGPU {
@@ -201,12 +89,11 @@ impl GMeanFilterGPU {
         if height.is_odd() {
             height -= 1
         }
-        if let Some(GPUComputeRunner { device, queue }) = GPUComputeRunner::new() {
+        if let Some(config) = GPUConfig::new() {
             Ok(Self {
                 x_radius: width / 2,
                 y_radius: height / 2,
-                device,
-                queue,
+                config,
             })
         } else {
             Err(String::from(
@@ -218,26 +105,6 @@ impl GMeanFilterGPU {
 
 impl Transformation for GMeanFilterGPU {
     fn apply(&self, image: &mut RgbImage) {
-        let out_image = create_image(
-            self.device.clone(),
-            &*self.queue,
-            image.width(),
-            image.height(),
-        )
-            .unwrap();
-
-        let out_image_view = ImageView::new_default(out_image.clone()).unwrap();
-
-        let in_image = create_image(
-            self.device.clone(),
-            &*self.queue,
-            image.width(),
-            image.height(),
-        )
-            .unwrap();
-
-        let in_image_view = ImageView::new_default(in_image.clone()).unwrap();
-
         mod cs {
             vulkano_shaders::shader! {
                 ty: "compute",
@@ -255,96 +122,15 @@ impl Transformation for GMeanFilterGPU {
             y_radius: self.y_radius,
         };
 
-        let shader: Arc<ShaderModule> =
-            cs::load(self.device.clone()).expect("Failed to create shader module");
+        let pipeline = InOutImageTransformationPipeline::new(
+            self.config.clone(),
+            image,
+            |device| cs::load(device).expect("Failed to create shader module"),
+            [image.width() / 16, image.height() / 16, 1],
+            Some(push_constants),
+        );
 
-        let compute_pipeline = ComputePipeline::new(
-            self.device.clone(),
-            shader.entry_point("main").unwrap(),
-            &(),
-            None,
-            |_| {},
-        )
-            .expect("Failed to create compute pipeline");
-
-        let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
-        let set = PersistentDescriptorSet::new(
-            layout.clone(),
-            [
-                WriteDescriptorSet::image_view(0, in_image_view.clone()),
-                WriteDescriptorSet::image_view(1, out_image_view.clone()),
-            ],
-        )
-            .unwrap();
-
-        let out_buf = CpuAccessibleBuffer::from_iter(
-            self.device.clone(),
-            BufferUsage {
-                transfer_dst: true,
-                ..Default::default()
-            },
-            false,
-            (0..image.width() * image.height() * 4).map(|_| 0u8),
-        )
-            .unwrap();
-
-        let input_rgba: Vec<u8> = DynamicImage::from(image.clone())
-            .to_rgba8()
-            .as_raw()
-            .clone();
-
-        let in_buf = CpuAccessibleBuffer::from_iter(
-            self.device.clone(),
-            BufferUsage {
-                transfer_src: true,
-                ..Default::default()
-            },
-            false,
-            input_rgba,
-        )
-            .unwrap();
-
-        let mut builder = AutoCommandBufferBuilder::primary(
-            self.device.clone(),
-            self.queue.queue_family_index(),
-            CommandBufferUsage::OneTimeSubmit,
-        )
-            .unwrap();
-
-        builder
-            .copy_buffer_to_image(CopyBufferToImageInfo::buffer_image(
-                in_buf.clone(),
-                in_image.clone(),
-            ))
-            .unwrap()
-            .bind_pipeline_compute(compute_pipeline.clone())
-            .bind_descriptor_sets(
-                PipelineBindPoint::Compute,
-                compute_pipeline.layout().clone(),
-                0,
-                set,
-            )
-            .push_constants(compute_pipeline.layout().clone(), 0, push_constants)
-            .dispatch([image.width() / 16, image.height() / 16, 1])
-            .unwrap()
-            .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
-                out_image.clone(),
-                out_buf.clone(),
-            ))
-            .unwrap();
-
-        let command_buffer = builder.build().unwrap();
-
-        let future = sync::now(self.device.clone())
-            .then_execute(self.queue.clone(), command_buffer)
-            .unwrap()
-            .then_signal_fence_and_flush()
-            .unwrap();
-
-        future.wait(None).unwrap();
-
-        let result_image = rgb_image_from_buffer(image.width(), image.height(), out_buf);
-
+        let result_image = pipeline.dispatch();
         *image = result_image;
     }
 }
