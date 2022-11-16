08c9f10e175ccd2f3c2fabea409cf608bfcb1e0d
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-16 12:27:50 +0100

    Further improved the efficiency of the median filter on the gpu

--- image_proc/src/shaders/median_filter.glsl
+++ image_proc/src/shaders/median_filter.glsl
@@ -12,21 +12,24 @@ layout (push_constant) uniform PushConstantData {
 layout (set = 0, binding = 0, rgba8) uniform readonly image2D inImage;
 layout (set = 0, binding = 1, rgba8) uniform writeonly image2D outImage;
 
-uint num_elements;
-uint luminosity_buckets[256];
+uint num_elements[3];
+uint luminosity_buckets[3][256];
 
 void clear_values() {
-    num_elements = 0;
-    for (int i = 0; i < 256; i++) {
-        luminosity_buckets[i] = 0;
+    for (int channel = 0; channel < 3; channel++) {
+        num_elements[channel] = 0;
+        for (int i = 0; i < 256; i++) {
+            luminosity_buckets[channel][i] = 0;
+        }
     }
+    
 }
 
-uint get_median() {
-    uint median_index = num_elements / 2;
+uint get_median(uint array[256], uint total_elements) {
+    uint median_index = total_elements / 2;
     uint current_index = 0;
     for (int luminosity = 0; luminosity < 256; luminosity++) {
-        current_index += luminosity_buckets[luminosity];
+        current_index += array[luminosity];
         if (current_index > median_index) {
             return uint(luminosity);
         }
@@ -51,27 +54,22 @@ void main() {
 
     clear_values();
     COLLECT_PIXELS (
-        uint luminosity = float_to_uint(pixel.r);
-        luminosity_buckets[luminosity]++;
-        num_elements++;
-        )
-    to_write.r = uint_to_float(get_median());
+        uint luminosity;
+        luminosity = float_to_uint(pixel.r);
+        luminosity_buckets[0][luminosity]++;
+        num_elements[0]++;
 
-    clear_values();
-    COLLECT_PIXELS (
-        uint luminosity = float_to_uint(pixel.g);
-        luminosity_buckets[luminosity]++;
-        num_elements++;
-        )
-    to_write.g = uint_to_float(get_median());
+        luminosity = float_to_uint(pixel.g);
+        luminosity_buckets[1][luminosity]++;
+        num_elements[1]++;
 
-    clear_values();
-    COLLECT_PIXELS (
-        uint luminosity = float_to_uint(pixel.b);
-        luminosity_buckets[luminosity]++;
-        num_elements++;
+        luminosity = float_to_uint(pixel.b);
+        luminosity_buckets[2][luminosity]++;
+        num_elements[2]++;
         )
-    to_write.b = uint_to_float(get_median());
+    to_write.r = uint_to_float(get_median(luminosity_buckets[0],num_elements[0]));
+    to_write.g = uint_to_float(get_median(luminosity_buckets[1],num_elements[1]));
+    to_write.b = uint_to_float(get_median(luminosity_buckets[2],num_elements[2]));
 
     imageStore(outImage, ID, vec4(to_write,1.0));
 }
