6d517720c66ef7bcaa088b7fd5353106504de7c1
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-16 12:12:44 +0100

    improved the efficiency of median filter

--- image_proc/src/shaders/median_filter.glsl
+++ image_proc/src/shaders/median_filter.glsl
@@ -1,6 +1,5 @@
 #version 450
 #include "shader_macros.h"
-#define MAX_SIZE 400
 
 layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
@@ -13,51 +12,66 @@ layout (push_constant) uniform PushConstantData {
 layout (set = 0, binding = 0, rgba8) uniform readonly image2D inImage;
 layout (set = 0, binding = 1, rgba8) uniform writeonly image2D outImage;
 
-uint numElements;
-float luminosities[MAX_SIZE];
+uint num_elements;
+uint luminosity_buckets[256];
 
-void sort() {
-    for ( int k=1; k < int( numElements); k++)
-    {
-        float temp = luminosities[k];
-        int j = k-1;
-        while (j>=0 && temp <= luminosities[j])
-        {
-            luminosities[j+1] = luminosities[j];
-            j = j-1;
-        }
-        luminosities[j+1] = temp;
+void clear_values() {
+    num_elements = 0;
+    for (int i = 0; i < 256; i++) {
+        luminosity_buckets[i] = 0;
     }
 }
+
+uint get_median() {
+    uint median_index = num_elements / 2;
+    uint current_index = 0;
+    for (int luminosity = 0; luminosity < 256; luminosity++) {
+        current_index += luminosity_buckets[luminosity];
+        if (current_index > median_index) {
+            return uint(luminosity);
+        }
+    }
+    return 255;
+}
+
+uint float_to_uint(float value) {
+    uint new_value = uint(value * 255.f + 0.1f);
+    return new_value <= 255 ? new_value : 255;
+}
+
+float uint_to_float(uint value) {
+    return float(value) / 255.f;
+}
+
 void main() {
     ivec2 max_size = imageSize(inImage);
     ivec2 ID = ivec2(gl_GlobalInvocationID.xy);
 
     vec3 to_write = vec3(0.0,0.0,0.0);
 
-    numElements = 0;
+    clear_values();
     COLLECT_PIXELS (
-        luminosities[numElements] = pixel.r;
-        numElements++;
+        uint luminosity = float_to_uint(pixel.r);
+        luminosity_buckets[luminosity]++;
+        num_elements++;
         )
-    sort();
-    to_write.r = luminosities[numElements / 2];
+    to_write.r = uint_to_float(get_median());
 
-    numElements = 0;
+    clear_values();
     COLLECT_PIXELS (
-        luminosities[numElements] = pixel.g;
-        numElements++;
+        uint luminosity = float_to_uint(pixel.g);
+        luminosity_buckets[luminosity]++;
+        num_elements++;
         )
-    sort();
-    to_write.g = luminosities[numElements / 2];
+    to_write.g = uint_to_float(get_median());
 
-    numElements = 0;
+    clear_values();
     COLLECT_PIXELS (
-        luminosities[numElements] = pixel.b;
-        numElements++;
+        uint luminosity = float_to_uint(pixel.b);
+        luminosity_buckets[luminosity]++;
+        num_elements++;
         )
-    sort();
-    to_write.b = luminosities[numElements / 2];
+    to_write.b = uint_to_float(get_median());
 
     imageStore(outImage, ID, vec4(to_write,1.0));
 }
