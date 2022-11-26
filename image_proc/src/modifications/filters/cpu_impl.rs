8d67aa8dacb66081cfbcfb3439246fe4a6198b89
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-11-26 23:26:29 +0100

    Added support for scaled masks

--- image_proc/src/modifications/filters/cpu_impl.rs
+++ image_proc/src/modifications/filters/cpu_impl.rs
@@ -187,21 +187,28 @@ impl Transformation for Uolis {
 }
 
 pub struct LowPassFilter {
-    mask: [[u8; 3]; 3],
+    mask: [[f64; 3]; 3],
+    mask_scale: f64,
 }
 
 impl LowPassFilter {
-    pub fn new(mask: [[u8; 3]; 3]) -> Self {
-        Self { mask }
+    pub fn new(mask: [[f64; 3]; 3], mask_scale: Option<f64>) -> Self {
+        Self {
+            mask,
+            mask_scale: mask_scale.unwrap_or(1.0),
+        }
     }
-    pub fn from_flat_mask(flat_mask: [u8; 9]) -> Self {
-        let mut mask = [[0u8; 3]; 3];
+    pub fn from_flat_mask(flat_mask: [f64; 9], mask_scale: Option<f64>) -> Self {
+        let mut mask = [[0f64; 3]; 3];
         for y in 0..3 {
             for x in 0..3 {
                 mask[y][x] = flat_mask[3 * y + x]
             }
         }
-        Self { mask }
+        Self {
+            mask,
+            mask_scale: mask_scale.unwrap_or(1.0),
+        }
     }
 }
 
@@ -214,14 +221,14 @@ impl Transformation for LowPassFilter {
             }
 
             for channel in 0..3 {
-                let mut sum = 0;
+                let mut sum = 0f64;
                 for i in 0..3 {
                     for j in 0..3 {
-                        sum += self.mask[j as usize][i as usize]
-                            * image.get_pixel(x + i - 1, y + j - 1)[channel];
+                        sum += (self.mask[j as usize][i as usize])
+                            * image.get_pixel(x + i - 1, y + j - 1)[channel] as f64;
                     }
                 }
-                pixel[channel] = sum;
+                pixel[channel] = (sum / self.mask_scale) as u8;
             }
         }
         *image = new_image;
