ccf17fa32985ca897ff770114b8873e752d7e23c
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-12-08 17:04:16 +0100

    Added ability to open a file

--- image_proc_gui/src-tauri/src/main.rs
+++ image_proc_gui/src-tauri/src/main.rs
@@ -3,7 +3,14 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use image;
+use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Submenu, Window, Wry};
+use tauri_api::dialog::Response::*;
+
+#[derive(Clone, serde::Serialize)]
+struct OpenFileEventArgs {
+    path: String,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,9 +18,25 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn open_file(window: &Window<Wry>) {
+    let filter_list: Option<&str> = None::<&str>;
+    let file = tauri_api::dialog::select(filter_list, None::<&str>);
+    if let Ok(Okay(file)) = file {
+        window
+            .emit_all("file-open", OpenFileEventArgs { path: file.clone() })
+            .unwrap();
+        let image = image::io::Reader::open(file)
+            .unwrap()
+            .decode()
+            .unwrap()
+            .to_rgb8();
+    }
+}
+
 fn main() {
     let quit = CustomMenuItem::new("quit", "Quit");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let open = CustomMenuItem::new("open", "Open");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(open));
     let menu = Menu::new()
         .add_native_item(MenuItem::Copy)
         .add_item(CustomMenuItem::new("hide", "Hide"))
@@ -23,6 +46,7 @@ fn main() {
         .menu(menu)
         .on_menu_event(|event| match event.menu_item_id() {
             "quit" => std::process::exit(0),
+            "open" => open_file(event.window()),
             _ => {}
         })
         .invoke_handler(tauri::generate_handler![greet])
--- image_proc_gui/src/App.svelte
+++ image_proc_gui/src/App.svelte
@@ -1,23 +1,62 @@
 <script lang="ts">
     import Greet from './lib/Greet.svelte'
+    import {tauri} from "@tauri-apps/api";
+    import {listen} from "@tauri-apps/api/event";
+    let img_src = "";
+    listen("file-open", (e) => {
+        let path: string = e.payload.path
+        loadImage(path)
+    })
+
+    function loadImage(path: string) {
+        img_src = tauri.convertFileSrc(path)
+    }
 </script>
 
 <main class="container">
-	<h1>Hello!</h1>
 
-	<div class="row">
-		<Greet />
+	<div class="image-view">
+		<!--suppress HtmlRequiredAltAttribute -->
+		<img id="active-image" src={img_src} alt="No image loaded. Use File->Open" />
+	</div>
+	<div class="column" id="adjustments-view">
+		<div class="histogram">Histogram</div>
+		<div class:Greet></div>
 	</div>
 
 
 </main>
 
 <style>
-	.logo.vite:hover {
-		filter: drop-shadow(0 0 2em #747BFF);
+	main {
+		flex-grow: 1;
+		padding: 0;
+		height: 98vh;
+		width: 99vw;
+		display: grid;
+		grid-template-columns: 3fr 1fr;
+		border: dashed gray 1px;
 	}
 
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #FF3E00);
+	#adjustments-view {
+		border: solid red 1px;
+		grid-column: 2;
+	}
+
+	.image-view {
+		border: solid 2px white;
+		grid-column: 1;
+		overflow: hidden;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.image-view > img {
+		position: relative;
+		max-height: 100%;
+		width: 100%;
+		object-fit: contain;
+		text-align: center;
 	}
 </style>
