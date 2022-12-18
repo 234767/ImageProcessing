3bc0a1ce03775572dd8fead78bf36890cec46920
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-12-18 17:43:47 +0100

    Added static methods on erosion and dilation

--- image_proc/src/modifications/morphological/dilation.rs
+++ image_proc/src/modifications/morphological/dilation.rs
@@ -11,21 +11,25 @@ impl Dilation {
     pub fn new(mask: Mask) -> Self {
         Self { mask }
     }
-}
 
-impl_transform!(Dilation);
-
-impl MorphologicalTransform for Dilation {
-    fn apply_morph_operation(&self, image: &mut GrayImage) {
+    pub(crate) fn apply(mask: &Mask, image: &mut GrayImage) {
         let mut new_image: GrayImage = ImageBuffer::new(image.width(), image.height());
         for (x, y, pixel) in image.enumerate_pixels() {
             if !is_foreground(pixel) {
                 continue;
             }
             let new_image_mask = &Mask::from_image(&new_image, x, y);
-            let mask = &self.mask | new_image_mask;
+            let mask = mask | new_image_mask;
             mask.write_to_image(&mut new_image, x, y);
         }
         *image = new_image;
+    }
+}
+
+impl_transform!(Dilation);
+
+impl MorphologicalTransform for Dilation {
+    fn apply_morph_operation(&self, image: &mut GrayImage) {
+        Self::apply(&self.mask, image);
     }
 }
