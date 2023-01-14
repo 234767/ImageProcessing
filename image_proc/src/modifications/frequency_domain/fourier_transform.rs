cf5c45aea932a182468e8acd96e518333b8318cf
Jakub Pawlak <234767@edu.p.lodz.pl>
2023-01-14 23:03:04 +0100

    Refactored the DFT to use f64 and iterators

--- image_proc/src/modifications/frequency_domain/fourier_transform.rs
+++ image_proc/src/modifications/frequency_domain/fourier_transform.rs
@@ -1,21 +1,22 @@
-use num::complex::{Complex, Complex32};
-use std::f32::consts::PI;
+use num::complex::{Complex, Complex64};
+use std::f64::consts::PI;
 
-pub fn dft_1d(samples: &[f32]) -> Vec<Complex32> {
+#[allow(non_snake_case)]
+#[inline(always)] // inline to allow for loop hoisting
+fn twiddle_factor(n: usize, k: usize, N: usize) -> Complex<f64> {
+    Complex::new(0.0, -2.0 * PI * n as f64 * (k as f64 / N as f64)).exp()
+}
+
+pub fn dft_1d(samples: &[f64]) -> Vec<Complex64> {
     let n = samples.len();
-    let mut result = Vec::new();
 
-    for k in 0..n {
-        let mut sum = Complex::new(0.0, 0.0);
-        for j in 0..n {
-            let frequency_factor = k as f32 / n as f32;
-            let exponent = -2.0 * PI * j as f32 * frequency_factor;
-            let direction_vector = Complex::new(0.0, exponent).exp();
-            sum += samples[j]
-                * direction_vector;
-        }
-        result.push(sum);
-    }
+    let result: Vec<_> = (0..n).map(|k|{
+        samples
+            .iter()
+            .enumerate()
+            .map(|(j, sample)| sample * twiddle_factor(j, k, n))
+            .sum()
+    }).collect();
 
     debug_assert_eq!(samples.len(), result.len());
     return result;
--- image_proc/tests/fourier_transform_tests/dft_1d_tests.rs
+++ image_proc/tests/fourier_transform_tests/dft_1d_tests.rs
@@ -1,7 +1,7 @@
 use image_proc::modifications::frequency_domain::fourier_transform::dft_1d;
 use num::Complex;
 
-const ACCURACY: f32 = 1e-6;
+const ACCURACY: f64 = 1e-6;
 
 macro_rules! assert_delta {
     ($x:expr, $y:expr, $d:expr) => {
@@ -11,7 +11,7 @@ macro_rules! assert_delta {
     };
 }
 
-fn test_dft_1d(input: &[f32], expected: &[(f32, f32)]) {
+fn test_dft_1d(input: &[f64], expected: &[(f64, f64)]) {
     let expected: Vec<_> = expected
         .iter()
         .map(|(re, im)| Complex::new(*re, *im))
